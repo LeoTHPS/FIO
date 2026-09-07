7e0a2595bbe322c269682772822cd977e6e81780
= <=>
2026-09-07 13:00:39 -0700

    add untested FIO::SerialPort

---
+++ FIO/SerialPort.cpp
@@ -0,0 +1,343 @@
+#include "SerialPort.hpp"
+
+#if defined(FIO_LINUX)
+	#define INVALID_SERIAL_PORT_HANDLE -1
+
+	#define GetLastError()             errno
+
+	#include <fcntl.h>
+	#include <unistd.h>
+	#include <termios.h>
+
+	#include <sys/ioctl.h>
+#elif defined(FIO_WIN32)
+	#define INVALID_SERIAL_PORT_HANDLE INVALID_HANDLE_VALUE
+#endif
+
+FIO::SerialPort::SerialPort(std::string_view path, uint32_t baud, int flags)
+	: is_open(false),
+	is_closing(false),
+	is_associated(false),
+	baud(baud),
+	flags(flags),
+#if defined(FIO_LINUX)
+	path(path),
+#elif defined(FIO_WIN32)
+	path(path.begin(), path.end()),
+#endif
+	error(0),
+	handle(INVALID_SERIAL_PORT_HANDLE),
+	thread_pool(nullptr)
+{
+}
+#if defined(FIO_WIN32)
+FIO::SerialPort::SerialPort(std::wstring_view path, uint32_t baud, int flags)
+	: is_open(false),
+	is_closing(false),
+	is_associated(false),
+	baud(baud),
+	flags(flags),
+	path(path),
+	error(0),
+	handle(INVALID_SERIAL_PORT_HANDLE),
+	thread_pool(nullptr)
+{
+}
+#endif
+
+FIO::SerialPort::~SerialPort()
+{
+	if (IsOpen())
+		Close();
+}
+
+bool FIO::SerialPort::Open()
+{
+	if (IsOpen())
+		return false;
+
+#if defined(FIO_LINUX)
+	// TODO: implement linux
+#elif defined(FIO_WIN32)
+	if ((handle = CreateFileW(GetPath().c_str(), GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0)) == INVALID_HANDLE_VALUE)
+	{
+		error = ::GetLastError();
+
+		return false;
+	}
+
+	DCB dcb = { .DCBlength = sizeof(DCB) };
+
+	if (!GetCommState(handle, &dcb))
+	{
+		error = ::GetLastError();
+
+		CloseHandle(handle);
+		handle = INVALID_SERIAL_PORT_HANDLE;
+
+		return false;
+	}
+
+	dcb.fBinary       = TRUE;
+	dcb.BaudRate      = (DWORD)GetBaud();
+	dcb.ByteSize      = 8;
+	dcb.StopBits      = ONESTOPBIT;
+	dcb.fAbortOnError = TRUE;
+
+	if (auto flags = GetFlags())
+	{
+		     if (flags & FLAG_PARITY_ODD)          { dcb.Parity = ODDPARITY;   dcb.fParity = TRUE;  }
+		else if (flags & FLAG_PARITY_EVEN)         { dcb.Parity = EVENPARITY;  dcb.fParity = TRUE;  }
+		else if (flags & FLAG_PARITY_MARK)         { dcb.Parity = MARKPARITY;  dcb.fParity = TRUE;  }
+		else if (flags & FLAG_PARITY_SPACE)        { dcb.Parity = SPACEPARITY; dcb.fParity = TRUE;  }
+		else if (flags & FLAG_PARITY_DISABLED)     { dcb.Parity = NOPARITY;    dcb.fParity = FALSE; }
+
+		if (flags & FLAG_TWO_STOP_BITS)              dcb.StopBits = TWOSTOPBITS;
+
+		     if (flags & FLAG_CTS_CONTROL_ENABLE)    dcb.fOutxCtsFlow = TRUE;
+		else if (flags & FLAG_CTS_CONTROL_ENABLE)    dcb.fOutxCtsFlow = FALSE;
+
+		     if (flags & FLAG_RTS_CONTROL_TOGGLE)    dcb.fRtsControl = RTS_CONTROL_TOGGLE;
+		else if (flags & FLAG_RTS_CONTROL_ENABLE)    dcb.fRtsControl = RTS_CONTROL_ENABLE;
+		else if (flags & FLAG_RTS_CONTROL_DISABLE)   dcb.fRtsControl = RTS_CONTROL_DISABLE;
+		else if (flags & FLAG_RTS_CONTROL_HANDSHAKE) dcb.fRtsControl = RTS_CONTROL_HANDSHAKE;
+	}
+
+	if (!SetCommState(handle, &dcb))
+	{
+		error = ::GetLastError();
+
+		CloseHandle(handle);
+		handle = INVALID_SERIAL_PORT_HANDLE;
+
+		return false;
+	}
+
+	COMMTIMEOUTS timeouts = { .ReadIntervalTimeout = MAXWORD };
+
+	if (!SetCommTimeouts(handle, &timeouts))
+	{
+		error = ::GetLastError();
+
+		CloseHandle(handle);
+		handle = INVALID_SERIAL_PORT_HANDLE;
+
+		return false;
+	}
+#endif
+
+	is_open = true;
+
+	return true;
+}
+void FIO::SerialPort::Close()
+{
+	if (IsOpen())
+	{
+		is_closing = true;
+
+		if (IsAssociated())
+			thread_pool_io.Wait();
+
+#if defined(FIO_LINUX)
+		close(handle);
+#elif defined(FIO_WIN32)
+		CloseHandle(handle);
+#endif
+
+		if (IsAssociated())
+			thread_pool_io.Wait();
+
+		error         = 0;
+		handle        = INVALID_SERIAL_PORT_HANDLE;
+		thread_pool   = nullptr;
+
+		is_open       = false;
+		is_closing    = false;
+		is_associated = false;
+	}
+}
+
+bool FIO::SerialPort::Associate(ThreadPool& pool)
+{
+	if (!IsOpen() || is_closing)
+		return false;
+
+#if defined(FIO_LINUX)
+	// TODO: implement linux
+#elif defined(FIO_WIN32)
+	if (!pool.Associate(GetHandle()))
+	{
+		error = pool.GetLastError();
+
+		return false;
+	}
+#endif
+
+	thread_pool   = &pool;
+	is_associated = true;
+
+	return true;
+}
+
+bool FIO::SerialPort::Read(void* buffer, size_t size, size_t& number_of_bytes_read)
+{
+	if (!IsOpen() || is_closing)
+		return false;
+
+#if defined(FIO_LINUX)
+	ssize_t num_bytes_read;
+
+	if ((num_bytes_read = read(GetHandle(), buffer, size)) == -1)
+	{
+		error = ::GetLastError();
+
+		return false;
+	}
+
+	number_of_bytes_read = num_bytes_read;
+#elif defined(FIO_WIN32)
+	DWORD num_bytes_read;
+
+	if (!ReadFile(GetHandle(), (LPVOID)buffer, (DWORD)size, &num_bytes_read, nullptr))
+	{
+		if ((error = ::GetLastError()) == ERROR_HANDLE_EOF)
+		{
+			number_of_bytes_read = 0;
+
+			return true;
+		}
+
+		return false;
+	}
+
+	number_of_bytes_read = num_bytes_read;
+#endif
+
+	return true;
+}
+bool FIO::SerialPort::Read(void* buffer, size_t size, ReadCallback&& callback)
+{
+	if (!IsOpen() || !IsAssociated() || is_closing)
+		return false;
+
+#if defined(FIO_LINUX)
+	// TODO: implement linux
+#elif defined(FIO_WIN32)
+	auto context = new IOContext_Read
+	{
+		.IO       = { .Callback = std::bind(&SerialPort::OnRead, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3) },
+		.Buffer   = { .Size = size, .Buffer = buffer },
+		.Callback = std::move(callback)
+	};
+
+	thread_pool_io.Add(context->IO);
+
+	if (!ReadFile(GetHandle(), (LPVOID)buffer, (DWORD)size, nullptr, &context->IO.O))
+		if ((error = ::GetLastError()) != ERROR_IO_PENDING)
+		{
+			thread_pool_io.Remove(context->IO);
+
+			delete context;
+
+			return false;
+		}
+#endif
+
+	return true;
+}
+
+bool FIO::SerialPort::Write(const void* buffer, size_t size, size_t& number_of_bytes_written)
+{
+	if (!IsOpen() || is_closing)
+		return false;
+
+#if defined(FIO_LINUX)
+	ssize_t num_bytes_written;
+
+	if ((num_bytes_written = write(GetHandle(), buffer, size)) == -1)
+	{
+		error = ::GetLastError();
+
+		return false;
+	}
+
+	number_of_bytes_written = num_bytes_written;
+#elif defined(FIO_WIN32)
+	DWORD num_bytes_written;
+
+	if (!WriteFile(GetHandle(), (LPCVOID)buffer, (DWORD)size, &num_bytes_written, nullptr))
+	{
+		error = ::GetLastError();
+
+		return false;
+	}
+
+	number_of_bytes_written = num_bytes_written;
+#endif
+
+	return true;
+}
+bool FIO::SerialPort::Write(const void* buffer, size_t size, WriteCallback&& callback)
+{
+	if (!IsOpen() || !IsAssociated() || is_closing)
+		return false;
+
+#if defined(FIO_LINUX)
+	// TODO: implement linux
+#elif defined(FIO_WIN32)
+	auto context = new IOContext_Write
+	{
+		.IO       = { .Callback = std::bind(&SerialPort::OnWrite, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3) },
+		.Buffer   = { .Size = size, .Buffer = buffer },
+		.Callback = std::move(callback)
+	};
+
+	thread_pool_io.Add(context->IO);
+
+	if (!WriteFile(GetHandle(), (LPCVOID)buffer, (DWORD)size, nullptr, &context->IO.O))
+		if ((error = ::GetLastError()) != ERROR_IO_PENDING)
+		{
+			thread_pool_io.Remove(context->IO);
+
+			delete context;
+
+			return false;
+		}
+#endif
+
+	return true;
+}
+
+void FIO::SerialPort::OnRead(ThreadPool& pool, ThreadPool::IOContext& io, size_t number_of_bytes_transferred)
+{
+	auto context = (IOContext_Read*)&io;
+
+#if defined(FIO_LINUX)
+	// TODO: implement linux
+#elif defined(FIO_WIN32)
+	this->error = context->IO.O.Internal;
+
+	context->Callback(*this, context->Buffer.Buffer, context->Buffer.Size, number_of_bytes_transferred);
+#endif
+
+	thread_pool_io.Remove(context->IO);
+
+	delete context;
+}
+void FIO::SerialPort::OnWrite(ThreadPool& pool, ThreadPool::IOContext& io, size_t number_of_bytes_transferred)
+{
+	auto context = (IOContext_Write*)&io;
+
+#if defined(FIO_LINUX)
+	// TODO: implement linux
+#elif defined(FIO_WIN32)
+	this->error = context->IO.O.Internal;
+
+	context->Callback(*this, context->Buffer.Buffer, context->Buffer.Size, number_of_bytes_transferred);
+#endif
+
+	thread_pool_io.Remove(context->IO);
+
+	delete context;
+}
---
+++ FIO/SerialPort.hpp
@@ -0,0 +1,156 @@
+#pragma once
+#include <string>
+#include <cstdint>
+#include <functional>
+
+#if defined(FIO_LINUX)
+
+#elif defined(FIO_WIN32)
+	#include <Windows.h>
+#endif
+
+#include "ThreadPool.hpp"
+
+namespace FIO
+{
+	class SerialPort
+	{
+	public:
+		enum FLAG
+		{
+			FLAG_PARITY_ODD            = 0x1,
+			FLAG_PARITY_EVEN           = 0x2,
+			FLAG_PARITY_MARK           = 0x4,
+			FLAG_PARITY_SPACE          = 0x8,
+			FLAG_PARITY_DISABLED       = 0x10,
+
+			FLAG_TWO_STOP_BITS         = 0x100,
+
+			FLAG_CTS_CONTROL_ENABLE    = 0x1000,
+			FLAG_CTS_CONTROL_DISABLE   = 0x2000,
+
+			FLAG_DTR_CONTROL_ENABLE    = 0x10000,
+			FLAG_DTR_CONTROL_DISABLE   = 0x20000,
+			FLAG_DTR_CONTROL_HANDSHAKE = 0x40000,
+
+			FLAG_RTS_CONTROL_TOGGLE    = 0x100000,
+			FLAG_RTS_CONTROL_ENABLE    = 0x200000,
+			FLAG_RTS_CONTROL_DISABLE   = 0x400000,
+			FLAG_RTS_CONTROL_HANDSHAKE = 0x800000
+		};
+
+		typedef std::function<void(SerialPort& port, void* buffer, size_t size, size_t number_of_bytes_read)>          ReadCallback;
+		typedef std::function<void(SerialPort& port, const void* buffer, size_t size, size_t number_of_bytes_written)> WriteCallback;
+
+	private:
+		struct IOContext_Read
+		{
+			struct BufferContext
+			{
+				size_t Size;
+				void*  Buffer;
+			};
+
+			ThreadPool::IOContext IO;
+			BufferContext         Buffer;
+			ReadCallback          Callback;
+		};
+		struct IOContext_Write
+		{
+			struct BufferContext
+			{
+				size_t      Size;
+				const void* Buffer;
+			};
+
+			ThreadPool::IOContext IO;
+			BufferContext         Buffer;
+			WriteCallback         Callback;
+		};
+
+		bool                  is_open;
+		bool                  is_closing;
+		bool                  is_associated;
+
+		uint32_t              baud;
+		int                   flags;
+#if defined(FIO_LINUX)
+		std::string           path;
+		std::atomic<int>      error;
+		int                   handle;
+#elif defined(FIO_WIN32)
+		std::wstring          path;
+		std::atomic<DWORD>    error;
+		HANDLE                handle;
+#endif
+
+		ThreadPool*           thread_pool;
+		ThreadPool::IOManager thread_pool_io;
+
+		SerialPort(SerialPort&&) = delete;
+		SerialPort(const SerialPort&) = delete;
+
+	public:
+		SerialPort(std::string_view path, uint32_t baud, int flags);
+#if defined(FIO_WIN32)
+		SerialPort(std::wstring_view path, uint32_t baud, int flags);
+#endif
+
+		virtual ~SerialPort();
+
+		constexpr bool  IsOpen() const
+		{
+			return is_open;
+		}
+
+		constexpr bool  IsAssociated() const
+		{
+			return is_associated;
+		}
+
+		constexpr auto  GetBaud() const
+		{
+			return baud;
+		}
+
+		constexpr auto& GetPath() const
+		{
+			return path;
+		}
+
+		constexpr auto  GetFlags() const
+		{
+			return flags;
+		}
+
+		constexpr auto  GetHandle() const
+		{
+			return handle;
+		}
+
+		inline    auto  GetLastError() const
+		{
+			return error.load();
+		}
+
+		constexpr auto  GetThreadPool() const
+		{
+			return thread_pool;
+		}
+
+		bool Open();
+		void Close();
+
+		bool Associate(ThreadPool& pool);
+
+		bool Read(void* buffer, size_t size, size_t& number_of_bytes_read);
+		bool Read(void* buffer, size_t size, ReadCallback&& callback);
+
+		bool Write(const void* buffer, size_t size, size_t& number_of_bytes_written);
+		bool Write(const void* buffer, size_t size, WriteCallback&& callback);
+
+	private:
+		void OnRead(ThreadPool& pool, ThreadPool::IOContext& io, size_t number_of_bytes_transferred);
+		void OnWrite(ThreadPool& pool, ThreadPool::IOContext& io, size_t number_of_bytes_transferred);
+	};
+}
